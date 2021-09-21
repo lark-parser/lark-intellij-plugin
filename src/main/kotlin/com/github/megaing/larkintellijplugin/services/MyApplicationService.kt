29aa2df895aa800cf929974052b9b64b8b31ba93
GitHub Action <action@github.com>
2021-09-21 10:55:48 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/megaing/larkintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.megaing.larkintellijplugin.services
+
+import com.github.megaing.larkintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/megaing/larkintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/megaing/larkintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.megaing.larkintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.megaing.larkintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
